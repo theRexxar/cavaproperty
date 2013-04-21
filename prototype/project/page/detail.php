f0880bfa58f36c368c759f9e8382dd8e02e7de46
Jepri Sinaga <therexxar@gmail.com>
2013-04-21 20:48:36 +0700

    update our project detail

--- prototype/project/page/detail.php
+++ prototype/project/page/detail.php
@@ -36,7 +36,93 @@
 						<p class=""><span class="mlr10">The upholstered seat sits on a simple precision-made steel base, and expertly sewn and fitted covers create a beautiful tailored appearance.</span></p>
 						<p class=""><span class="mlr10">A matching Ottoman compliments the aesthetic of the chair.</span></p>
 
-						<p class="clear left mt20"><a href="<?php echo $base_url ?>" class="link-button green left">FINDER</a></p>
+						<div id="finder-container">
+							<form action="http://localhost/cava/beta/public/projects/search" methode="GET">
+								<h5>SEARCH PROJECT</h5>
+								<div class="input">
+									<select name="type" id="">
+										<option value="">Type Property</option>
+										<option value="residential">Residential</option>
+										<optgroup label="">
+											<option value="res-apartement">Apartement</option>
+											<option value="res-condotel">Condotel</option>
+											<option value="res-house">House</option>
+											<option value="res-villa">Villa</option>
+										</optgroup>
+										<option value="commercial">Commercial</option>
+										<optgroup label="">
+											<option value="com-shop">Shop (Ruko)</option>
+											<option value="com-office">Office</option>
+											<option value="com-factory">Factory</option>
+											<option value="com-warehouse">Warehouse</option>
+										</optgroup>
+									</select>	
+								</div>
+								<div class="input">
+									<select name="location" id="">
+										<option value="">Location</option>
+										<option value="ID-AC">Aceh</option>
+										<option value="ID-BA">Bali</option>
+										<option value="ID-BT">Banten</option>
+										<option value="ID-BE">Bengkulu</option>
+										<option value="ID-GO">Gorontalo</option>
+										<option value="ID-JK">Jakarta</option>
+										<option value="ID-JA">Jambi</option>
+										<option value="ID-JB">Jawa-Barat</option>
+										<option value="ID-JT">Jawa-Tengah</option>
+										<option value="ID-JI">Jawa-Timur</option>
+										<option value="ID-KB">Kalimantan-Barat</option>
+										<option value="ID-KS">Kalimantan-Selatan</option>
+										<option value="ID-KT">Kalimantan-Tengah</option>
+										<option value="ID-KI">Kalimantan-Timur</option>
+										<option value="ID-KU">Kalimantan-Utara</option>
+										<option value="ID-BB">Kepulauan-Bangka-Belitung</option>
+										<option value="ID-KR">Kepulauan-Riau</option>
+										<option value="ID-LA">Lampung</option>
+										<option value="ID-MA">Maluku</option>
+										<option value="ID-MU">Maluku-Utara</option>
+										<option value="ID-NB">Nusa-Tenggara-Barat</option>
+										<option value="ID-NT">Nusa-Tenggara-Timur</option>
+										<option value="ID-PA">Papua</option>
+										<option value="ID-PB">Papua-Barat</option>
+										<option value="ID-RI">Riau</option>
+										<option value="ID-SR">Sulawesi-Barat</option>
+										<option value="ID-ST">Sulawesi-Selatan</option>
+										<option value="ID-SN">Sulawesi-Tengah</option>
+										<option value="ID-SG">Sulawesi-Tenggara</option>
+										<option value="ID-SA">Sulawesi-Utara</option>
+										<option value="ID-SB">Sumatera-Barat</option>
+										<option value="ID-SS">Sumatera-Selatan</option>
+										<option value="ID-SU">Sumatera-Utara</option>
+										<option value="ID-YO">Yogyakarta</option>
+									</select>
+								</div>
+								<div class="input">
+									<select name="bedroom" id="">
+										<option value="0">Beedrooms</option>
+										<option value="1">1 Beedrooms</option>
+										<option value="2">2 Beedrooms</option>
+										<option value="3">3 Beedrooms</option>
+										<option value="4">4 Beedrooms</option>
+										<option value="5">5 Beedrooms</option>
+										<option value="6">6 Beedrooms</option>
+										<option value="7">7 Beedrooms</option>
+										<option value="8">8 Beedrooms</option>
+										<option value="9">9 Beedrooms</option>
+										<option value="10">10+ Beedrooms</option>
+									</select>
+								</div>
+								<div class="input">
+									<input type="text" placeholder="Search" value="" id="" name="q" class="">
+								</div>
+								<div class="submit">
+									<input type="submit" class="button radius" value="FIND PROJECT">
+								</div>							
+							</form>								
+							<a href="#" class="close">x</a>
+						</div>
+
+						<p class="clear left mt20"><a href="<?php echo $base_url ?>/includes/finder.php" class="link-button green left" id="finder">FINDER</a></p>
 					</div>
 				</div>
 			</section>	
