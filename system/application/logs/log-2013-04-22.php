19b4ea63a620255dba52b08f8e9af5f436904906
U-Jepri-PC\Jepri <Jepri@Jepri-PC.(none)>
2013-04-22 13:58:36 +0700

    update css

---
+++ system/application/logs/log-2013-04-22.php
@@ -0,0 +1,13 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-04-22 05:57:01 --> Severity: Notice  --> Undefined variable: banner C:\xampp\htdocs\cavaproperty\system\modules\search\controllers\search.php 57
+ERROR - 2013-04-22 05:57:09 --> Severity: Notice  --> Undefined variable: banner C:\xampp\htdocs\cavaproperty\system\modules\search\controllers\search.php 57
+ERROR - 2013-04-22 06:10:52 --> Severity: Notice  --> Undefined variable: banner C:\xampp\htdocs\cavaproperty\system\modules\search\controllers\search.php 57
+ERROR - 2013-04-22 06:10:55 --> Severity: Notice  --> Undefined variable: banner C:\xampp\htdocs\cavaproperty\system\modules\search\controllers\search.php 57
+ERROR - 2013-04-22 06:20:40 --> Severity: Notice  --> Undefined offset: 2014 C:\xampp\htdocs\cavaproperty\system\modules\news\views\front_page\index.php 11
+ERROR - 2013-04-22 06:20:40 --> Severity: Notice  --> Undefined offset: 7 C:\xampp\htdocs\cavaproperty\system\modules\news\views\front_page\index.php 11
+ERROR - 2013-04-22 06:20:40 --> Severity: Notice  --> Undefined offset: 6 C:\xampp\htdocs\cavaproperty\system\modules\news\views\front_page\index.php 11
+ERROR - 2013-04-22 06:20:40 --> Severity: Notice  --> Undefined offset: 2013 C:\xampp\htdocs\cavaproperty\system\modules\news\views\front_page\index.php 11
+ERROR - 2013-04-22 06:20:40 --> Severity: Notice  --> Undefined offset: 5 C:\xampp\htdocs\cavaproperty\system\modules\news\views\front_page\index.php 11
+ERROR - 2013-04-22 06:20:40 --> Severity: Notice  --> Undefined offset: 4 C:\xampp\htdocs\cavaproperty\system\modules\news\views\front_page\index.php 11
+ERROR - 2013-04-22 06:20:40 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at C:\xampp\htdocs\cavaproperty\system\modules\files\controllers\files.php:203) C:\xampp\htdocs\cavaproperty\system\codeigniter\libraries\Session.php 672
